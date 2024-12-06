f087892e5b76929f16aa706b8775ba73fd0829f2
alinepmarcondes <aline.patta@ges.inatel.br>
2024-12-06 14:29:35 -0300

    fix: Alteração de "produto" para "produtos"

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS "vendas";
-DROP TABLE IF EXISTS "produto";
+DROP TABLE IF EXISTS "produtos";
 
-CREATE TABLE "produto" (
+CREATE TABLE "produtos" (
     "id" SERIAL PRIMARY KEY,
     "nome" VARCHAR(255) NOT NULL,
     "marca" VARCHAR(255) NOT NULL,
@@ -13,20 +13,20 @@ CREATE TABLE "produto" (
 
 CREATE TABLE "vendas" (
     "id" SERIAL PRIMARY KEY,
-    "produto_id" INTEGER REFERENCES produto(id) ON DELETE CASCADE,
+    "produto_id" INTEGER REFERENCES produtos(id) ON DELETE CASCADE,
     "quantidade_vendida" INTEGER NOT NULL,
     "valor_venda" FLOAT NOT NULL,
     "data_venda" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Ração', 'Purina', 'Cachorro', '1-2 anos', 50, 100.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Brinquedo de Borracha', 'Kong', 'Cachorro', '3-5 anos', 30, 25.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Ração Premium', 'Whiskas', 'Gato', '1-2 anos', 40, 120.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Areia Sanitária', 'Pipicat', 'Gato', 'Adulto', 60, 35.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Coleira com Guia', 'Ferplast', 'Cachorro', '6 meses - 1 ano', 20, 45.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Shampoo Neutro', 'Pet Society', 'Cachorro', 'Filhote', 25, 30.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Ração Especial', 'Alcon', 'Pássaro', 'Adulto', 15, 50.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Petisco Natural', 'Pedigree', 'Cachorro', '2-3 anos', 50, 20.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Cama Confortável', 'Truqys', 'Gato', 'Adulto', 10, 150.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Brinquedo de Pelúcia', 'Chalesco', 'Cachorro', 'Filhote', 35, 40.00);
-INSERT INTO "produto" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Comedouro de Inox', 'PetFlex', 'Gato', 'Todos', 20, 25.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Ração', 'Purina', 'Cachorro', '1-2 anos', 50, 100.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Brinquedo de Borracha', 'Kong', 'Cachorro', '3-5 anos', 30, 25.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Ração Premium', 'Whiskas', 'Gato', '1-2 anos', 40, 120.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Areia Sanitária', 'Pipicat', 'Gato', 'Adulto', 60, 35.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Coleira com Guia', 'Ferplast', 'Cachorro', '6 meses - 1 ano', 20, 45.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Shampoo Neutro', 'Pet Society', 'Cachorro', 'Filhote', 25, 30.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Ração Especial', 'Alcon', 'Pássaro', 'Adulto', 15, 50.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Petisco Natural', 'Pedigree', 'Cachorro', '2-3 anos', 50, 20.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Cama Confortável', 'Truqys', 'Gato', 'Adulto', 10, 150.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Brinquedo de Pelúcia', 'Chalesco', 'Cachorro', 'Filhote', 35, 40.00);
+INSERT INTO "produtos" ("nome", "marca", "animal", "idade", "quantidade", "preco") VALUES ('Comedouro de Inox', 'PetFlex', 'Gato', 'Todos', 20, 25.00);
